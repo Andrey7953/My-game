f511942ffea3254652fa3f31c3d7cb97faa09acd
Andrey7953 <165550423+Andrey7953@users.noreply.github.com>
2024-04-21 12:29:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from pygame import *
-
+score = 0
+lost = 0
+font.init()
+font2 = font.Font(None,36)
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y,player_speed):
+    def __init__(self, player_image, player_x, player_y,player_speed, player_hight, player_width):
         super().__init__()
-        self.image= transform.scale(image.load(player_image), (65, 65))
+        self.image= transform.scale(image.load(player_image), (player_hight, player_width))
         self.speed = player_speed
+        self.speedX = player_speed
+        self.speedY = player_speed
         self.rect =self.image.get_rect()
         self.rect.x=player_x
         self.rect.y=player_y
@@ -29,15 +34,31 @@ class Enemy(GameSprite):
 
 class Enemy2(GameSprite):
     def update(self):
-        self.rect.x += self.speed
+        global finish
+        self.rect.x += self.speedX
+        self.rect.y += self.speedY
+    
+        if self.rect.y >= 450:
+            self.speedY *= -1
+        if self.rect.y <= 0:
+            self.speedY *= -1
+
+
         if sprite.collide_rect(sprite2, sprite3):
-            self.speed *= -1
+            self.speedX *= -1
         if sprite.collide_rect(sprite1, sprite3):
-            self.speed *= -1
+            self.speedX *= -1
+        
+        if self.rect.x >= 650:
+            finish = True
+            
+        if self.rect.x <= 0:
+            finish = True
+            
 clock = time.Clock()
 window = display.set_mode((700,500))
 display.set_caption('Пинг понг')
-background = transform.scale(image.load('Без имени3.png'), (700,500))
+background = transform.scale(image.load('Без имени3.png'), (1000,500))
 
 x2= 200
 y2=100
@@ -45,39 +66,40 @@ x1= 350
 y1= 250
 x3=300
 y3=350
-sprite1 = Player('Без имени.png', 20, 200, 2)
-sprite2 = Enemy('Без имени2.png', 620, 200 ,2)
-sprite3 = Enemy2('gratis-png-ping-pong-thumbnail.png', 300, 200, 3)
+sprite1 = Player('Без имени.png', 20, 200, 3, 65,150)
+sprite2 = Enemy('Без имени2.png', 620, 200 ,3,65,150)
+sprite3 = Enemy2('gratis-png-ping-pong-thumbnail.png', 300, 200, 3,65,65)
 
-
+font.init()
+font3 = font.Font(None, 80)
 clock = time.Clock()
 FPS = 120
 
 
+
 Game = True
+finish = False
+
 while Game:
-    window.blit(background,(0,0))
-    sprite1.reset()
-    sprite1.update()
-    sprite2.reset()
-    sprite2.update()
-    sprite3.reset()
-    sprite3.update()
+    if finish == False:
+        window.blit(background,(0,0))
+        sprite1.reset()
+        sprite1.update()
+        sprite2.reset()
+        sprite2.update()
+        sprite3.reset()
+        sprite3.update()
+    else:
+        text3 = font3.render('Проиграл',1,(255,77,255))
+        window.blit(text3,(250,250))
+
+    text1 = font2.render('Счёт:'+ str(score),1,(255,60, 255))
+    text2 = font2.render('Пропущено:'+ str(lost),1,(255,60,255))
+    window.blit(text1,(10,20))
+    window.blit(text2,(10,50))
     for e in event.get():
         if e.type == QUIT:
             Game = False
     display.update()
     clock.tick(FPS)
 
-Game = True
-while game:
-    window.blit(background,(0,0))
-    sprite1.reset()
-    sprite2.reset()
-    sprite1.update()
-    sprite2.update()
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-    display.update()
-    clock.tick(FPS)
